b4f3a5d95a2d183f84297c7321aeb25c788c845d
halil <halil@DESKTOP-7IOJAC9>
2021-09-04 18:21:38 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            Kurs kurs1 = new Kurs();
+            kurs1.kursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.IzlenmeOrani = 65;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.kursAdi = "Java";
+            kurs2.Egitmen = "Kerem Varış";
+            kurs2.IzlenmeOrani = 75;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.kursAdi = "Python";
+            kurs3.Egitmen = "Berkay Bilgin";
+            kurs3.IzlenmeOrani = 80;
+
+            Kurs[] kurslar = new Kurs[] {kurs1, kurs2, kurs3 };
+
+            foreach (Kurs item in kurslar)
+            {
+                Console.WriteLine("Kurs adı : " + item.kursAdi + "  -  " + "eğitmen : " + item.Egitmen);               
+            }
+        }
+    }
+
+    class Kurs
+    {
+        public string kursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
